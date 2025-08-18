40fe47fa3b6cf42228a4b79bc3b5f82d432533b7
Josue Lopez <joshu_149@hotmail.com>
2025-08-18 13:44:47 -0400

    update to guess game

--- RandomNumb.py
+++ RandomNumb.py
@@ -23,14 +23,22 @@ print(choice)
 guesses = 0
 
 while True:
-    guess = int(input(f"Guess a number between {low} and {high}: "))
-    guesses += 1
-    if guess > numb:
-        print("Too high!")
-    elif guess < numb:
-        print("Too low!")
+    guess = input(f"Guess a number between {low} and {high}: ")
+    if guess.isdigit():
+        guess = int(guess)
+        guesses += 1
+        if guess > high or guess < low:
+            print("Out of bounds! Try again.")
+            print(f"Please select a number between {low} and {high}.")
+        elif guess > numb:
+            print("Too high!")
+        elif guess < numb:
+            print("Too low!")
+        else:
+            print(f"That is correct! The number was {numb}.")
+            break
     else:
-        print(f"That is correct! The number was {numb}.")
-        break
+        print("That is not a number! Please try again.")
+        print(f"Please select a number between {low} and {high}.")
 
 print(f"You guessed it in {guesses} tries.")
