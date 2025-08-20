c94d555934ef21794d7a4485a652b5e5d0ede362
Josue Lopez <joshu_149@hotmail.com>
2025-08-20 11:39:06 -0400

    kwargs practice

--- Func.py
+++ Func.py
@@ -12,3 +12,40 @@ def count(start, end):
     print("DONE!")
 
 count(0, 5)
+
+def get_phone(country, area, first, last):
+    return f"+{country}-{area}-{first}-{last}"
+
+print(get_phone(first=1234, last=5678, country=1, area=800))
+
+
+def add(*args): 
+    total = 0
+    for arg in args:
+        total += arg
+    return total
+
+print(add(1, 2, 3, 4, 5))
+
+def shipping_address(*args, **kwargs):
+    for arg in args:
+        print(arg, end='')
+        print()
+
+    if "apts" in kwargs:
+        print(f"{kwargs.get('street')} {kwargs.get('apts')}")
+
+    else:
+        print(f"{kwargs.get('street')}")
+
+    print(f"{kwargs.get('city')}, {kwargs.get('state')} {kwargs.get('zip')}")
+
+shipping_address(
+    "John",
+    "Doe",
+    street="123 Main St",
+    apts="Apt 4B",
+    city="New York",
+    state="NY",
+    zip="10001"
+)
