17d91da84f5cea0928e74f0857ddc99dd59097e0
Josue Lopez <joshu_149@hotmail.com>
2025-08-21 12:50:07 -0400

    Credit card validation code

---
+++ CreditC_Validation.py
@@ -0,0 +1,26 @@
+card_numb = input("Enter your credit card number: ")
+#card_numb = (numb for numb in card_numb if numb != '-')
+#print("".join(card_numb))
+total = 0
+sum_even_numb = 0
+sum_odd_numb = 0
+
+card_numb = card_numb.replace("-", "")
+card_numb = card_numb.replace(" ", "")
+
+for num in card_numb[::2]: 
+    sum_odd_numb += int(num)
+
+for num in card_numb[1::2]: 
+    x = int(num) * 2
+    if x >= 10:
+        sum_even_numb += (1 +(x % 10))
+    else:
+        sum_even_numb += x
+
+total = sum_even_numb + sum_odd_numb
+
+if total % 10 == 0:
+    print("The credit card number is valid.")
+else:
+    print("The credit card number is invalid.")
