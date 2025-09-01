a7ae532e35b2ddae18d13ec10e85dfb73ff23ee1
Josue L <joshu_149@hotmail.com>
2025-09-01 00:06:45 -0400

    testing edge cases

--- unit_test/unexpected.py
+++ unit_test/unexpected.py
@@ -8,6 +8,15 @@ def divide(a, b):
   return a / b
 
 class TestUnexpected(unittest.TestCase):
-  def not_a_numb(self):
+  
+  def test_not_a_positive_numb(self):
+    with self.assertRaises(ValueError):
+      self.assertEqual(12, get_sqrt(-144))
+
+  def test_not_divide_by_zero(self):
     with self.assertRaises(ZeroDivisionError):
-      self.
+      self.assertEqual(12, divide(144, 0))
+
+
+if __name__ == "__main__":
+  unittest.main()
