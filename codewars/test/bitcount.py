5ed01ef9cc26299e7fc44865300a59141d40206f
Josue L <joshu_149@hotmail.com>
2025-09-03 14:09:29 -0400

    practice of codewars

--- codewars/test/bitcount.py
+++ codewars/test/bitcount.py
@@ -5,3 +5,9 @@ def count_bit(number):
         sum += int(num)
     return sum
 
+#Otra forma de escribir esto seria 
+
+def new_count_bits(number):
+    return bin(number).count("1")
+
+print(new_count_bits(1234))
--- codewars/test/test_bitcount.py
+++ codewars/test/test_bitcount.py
@@ -1,11 +1,16 @@
 import unittest
 from bitcount import count_bit
+from bitcount import new_count_bits
 
 class TestBits(unittest.TestCase):
 
     def test_bit_count(self):
         self.number = 1234
         self.assertEqual(5, count_bit(self.number))
+
+    def test_new_count_bits(self):
+        self.number = 1234
+        self.assertEqual(5, new_count_bits(self.number))
 
 if __name__ == "__main__":
     unittest.main()
